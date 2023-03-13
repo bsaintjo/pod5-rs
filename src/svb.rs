23a05b5303a3ea1480c265fcb93a3294bb972fbe
Brandon Saint-John (aarch64) <bsaintjo@ucsc.edu>
2023-03-13 10:55:20 -0700

    try to implement vbz decompression

--- src/svb.rs
+++ src/svb.rs
@@ -1,21 +1,17 @@
-fn decode_data(data: &[u8], code: u8) {
-    todo!()
-}
+use std::io::Read;
 
-fn decode_scalar(out: &[i16], keys: &[u8], data: &[u8], count: u32, prev: i16) {
-    if count == 0 {
-        todo!()
-    }
-    let mut shift = 0u8;
-    let mut key_byte = todo!();
-    let u_prev = prev as u16;
-    for c in 0..count {
-        shift += 1;
-        if shift == 8 {
-            shift = 0;
-            key_byte = todo!();
-        }
-        todo!()
-    }
-    todo!()
+use delta_encoding::DeltaDecoderExt;
+use stream_vbyte::scalar::Scalar;
+use zstd::decode_all;
+use zigzag::ZigZag;
+
+fn decode<R>(source: R) -> eyre::Result<Vec<i16>>
+where
+    R: Read,
+{
+    let zstd_decoded = decode_all(source)?;
+    let mut svb_decoded = Vec::new();
+    let _svb_decoded_count = stream_vbyte::decode::decode::<Scalar>(&zstd_decoded, zstd_decoded.len(), &mut svb_decoded);
+    let delta_zz_decoded = svb_decoded.into_iter().map(|x| i16::decode(x as u16)).original().collect::<Vec<_>>();
+    Ok(delta_zz_decoded)
 }
