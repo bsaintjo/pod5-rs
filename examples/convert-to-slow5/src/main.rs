f2b82a545b9740c97f8e5ec2ccf476f4cc936e85
Brandon Saint-John <bsaintjo@ucsc.edu>
2024-03-11 01:38:46 -0700

    Convert to workspace, start working on example

--- examples/convert-to-slow5/src/main.rs
+++ examples/convert-to-slow5/src/main.rs
@@ -1,5 +1,16 @@
-use pod5;
+use std::path::PathBuf;
 
-fn main() {
-    println!("Hello, world!");
+use pico_args::Arguments;
+use pod5;
+use slow5;
+
+fn run(path: PathBuf) -> eyre::Result<()> {
+    todo!()
+}
+
+fn main() -> eyre::Result<()> {
+    let mut args = Arguments::from_env();
+    let path: PathBuf = args.free_from_str()?;
+    run(path)?;
+    Ok(())
 }
