1fab2c49a20e82ea718ee1abe1647b89baf7d5e2
Brandon Saint-John <bsaintjo@ucsc.edu>
2024-09-15 20:57:10 -0700

    Expand convert-to-slow5

--- examples/convert-to-slow5/src/main.rs
+++ examples/convert-to-slow5/src/main.rs
@@ -1,16 +1,55 @@
-use std::path::PathBuf;
+use std::{error::Error, fs::File, path::PathBuf};
 
+use eyre::eyre;
 use pico_args::Arguments;
-use pod5;
-use slow5;
+use pod5::{self, polars::prelude::AnyValue, reader::Reader};
+use slow5::{self, FileWriter};
 
-fn run(path: PathBuf) -> eyre::Result<()> {
-    todo!()
+fn run(path: PathBuf, output: PathBuf) -> eyre::Result<()> {
+    let file = File::open(path)?;
+    let output = FileWriter::create(output)?;
+
+    let mut reader = Reader::from_reader(file)?;
+    let mut read_iter = reader.read_dfs()?;
+    let next = read_iter.next().ok_or(eyre::eyre!("Reading went bad"))??;
+    let binding = next.into_inner();
+
+    #[allow(clippy::never_loop)]
+    for row_idx in 0..binding.height() {
+        if let Some(row) = binding.get(row_idx) {
+            println!("{row:?}");
+            let AnyValue::Binary(row_id) = row[0] else {
+                return Err(eyre!("id"));
+            };
+        }
+        break;
+    }
+    // for x in next.into_inner().iter_chunks(false) {
+    //     println!("{x:?}");
+    // }
+    Ok(())
 }
 
 fn main() -> eyre::Result<()> {
     let mut args = Arguments::from_env();
-    let path: PathBuf = args.free_from_str()?;
-    run(path)?;
+    let input: PathBuf = args.free_from_str()?;
+    let output: PathBuf = args.free_from_str()?;
+    run(input, output)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::env::temp_dir;
+
+    use super::*;
+
+    #[test]
+    fn test_conversion() {
+        let path = PathBuf::from("../../extra/multi_fast5_zip_v3.pod5");
+        let output = temp_dir().join("output.slow5");
+        if let Err(e) = run(path, output) {
+            println!("{e:?}");
+        }
+    }
 }
