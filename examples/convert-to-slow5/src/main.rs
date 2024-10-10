7157eb84306843add880cf00a3921dd4d302ad98
Brandon Saint-John <bsaintjo@ucsc.edu>
2024-10-10 05:40:54 -0700

    Use iflet to ease extraction

--- examples/convert-to-slow5/src/main.rs
+++ examples/convert-to-slow5/src/main.rs
@@ -1,6 +1,7 @@
 use std::{error::Error, fs::File, path::PathBuf};
 
 use eyre::eyre;
+use if_chain::if_chain;
 use pico_args::Arguments;
 use pod5::{self, polars::prelude::AnyValue, reader::Reader};
 use slow5::{self, FileWriter};
@@ -13,14 +14,20 @@ fn run(path: PathBuf, output: PathBuf) -> eyre::Result<()> {
     let mut read_iter = reader.read_dfs()?;
     let next = read_iter.next().ok_or(eyre::eyre!("Reading went bad"))??;
     let binding = next.into_inner();
+    println!("{binding:?}");
 
     #[allow(clippy::never_loop)]
     for row_idx in 0..binding.height() {
         if let Some(row) = binding.get(row_idx) {
             println!("{row:?}");
-            let AnyValue::Binary(row_id) = row[0] else {
-                return Err(eyre!("id"));
-            };
+            if_chain! {
+                if let AnyValue::Binary(row_id) = row[0];
+                then {
+                    dbg!("row_id: {row_id:?}");
+                } else {
+                    return Err(eyre!("id"));
+                }
+            }
         }
         break;
     }
