360f6d3d563e27f71b736a893cd375ddf16a1f63
Brandon Saint-John (aarch64) <bsaintjo@ucsc.edu>
2026-02-08 13:24:27 -0800

    read table buf

--- pod5-tokio/src/lib.rs
+++ pod5-tokio/src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+struct Reader;
 
 #[cfg(test)]
-mod tests {
+mod test {
+    use pod5_format::ParsedFooter;
+    use tokio::{fs::File, io::AsyncReadExt};
+
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn test_pod5_tokio() {
+        let path = "../extra/multi_fast5_zip_v3.pod5";
+        let mut file = File::open(path).await.unwrap();
+
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf).await.unwrap();
+
+        let footer = ParsedFooter::read_footer(&buf).unwrap();
+        let mut read_table_buf = Vec::new();
+        let read_table = footer.read_table().unwrap();
+        read_table.read_to_buf(&buf, &mut read_table_buf).unwrap();
+        
     }
 }
